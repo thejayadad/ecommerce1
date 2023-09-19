8e135afd6a0b1ba3730ed482cd86fa44dfa468db
thejayadad <thejayadad@gmail.com>
2023-09-19 06:06:16 -0400

    udpate

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import Product from "@/components/Product/Product";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Product />
     </main>
   )
 }
---
+++ myapp/components/Product/Product.js
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react'
+
+const Product = () => {
+  return (
+    <section>
+        <div className='w-screen flex flex-col items-center justify-center relative !p-0'>
+        <div className='bg-zinc-900 w-full flex justify-center'>
+        <h3
+        className='w-full max-w-7xl mb-[6px] md:mb-[10px] text-white pt-12 md:pt-20 px-4 text-4xl md:text-7xl'
+        >
+            Official Shop
+        </h3>
+        </div>
+        </div>
+    </section>
+  )
+}
+
+export default Product
